e5cc9ddf7c55c4037f74a40d103eda48552e7d97
Márton Szabó <makotoimx@gmail.com>
2021-02-23 10:53:51 +0100

    Add Api key and baseurl fields for URL Creator

--- App/Services/RequestServices/UrlCreator.cs
+++ App/Services/RequestServices/UrlCreator.cs
@@ -1,7 +1,11 @@
 namespace App.Services.RequestServices
 {
+
     public class UrlCreator : IUrlCreator
     {
+
+         private const string ApiKey = "7a75b617-4388-40dc-80f3-b15f4e0e42f6", BaseURL = "https://content.guardianapis.com/search?";
+
         public void CreateURL(string parameters)
         {
             throw new System.NotImplementedException();
