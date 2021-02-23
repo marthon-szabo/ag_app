0ae231dee5baba0bc7aec0eb4f67bac648a5cf8a
Márton Szabó <makotoimx@gmail.com>
2021-02-23 10:51:19 +0100

    Create concrete class for UrlCreator

---
+++ App/Services/RequestServices/UrlCreator.cs
@@ -0,0 +1,10 @@
+namespace App.Services.RequestServices
+{
+    public class UrlCreator : IUrlCreator
+    {
+        public void CreateURL(string parameters)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
