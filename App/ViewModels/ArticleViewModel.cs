929776872221d807d2af6d1855b1897a5042549f
Márton Szabó <makotoimx@gmail.com>
2021-02-23 13:49:19 +0100

    Create ArticleViewModel

--- App/ViewModels/ArticleViewModel.cs
+++ App/ViewModels/ArticleViewModel.cs
@@ -1,7 +1,17 @@
+using System;
+using System.Collections.Generic;
+
 namespace App.ViewModels
 {
     public struct ArticleViewModel
     {
-        
+        public string Id { get; set; }
+        public IEnumerable<ArticleAssetViewModel> Assets { get; set; }
+        public string BodyHtml { get; set; }
+        public string Type { get; set; }
+        public string SectionName { get; set; }
+        public DateTime WebPublicationDate { get; set; }
+        public DateTime WebTitle { get; set; }
+
     }
 }
