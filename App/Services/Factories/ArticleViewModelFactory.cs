7af19ec2ec6e926f812e8c3b0ea9ba61dd375e4a
Márton Szabó <makotoimx@gmail.com>
2021-02-23 14:07:55 +0100

    Add ArticleViewModelFactory.CreateResultVM()

--- App/Services/Factories/ArticleViewModelFactory.cs
+++ App/Services/Factories/ArticleViewModelFactory.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
+using System.Text.Json;
 using App.Services.Factories.Interfaces;
 using App.ViewModels;
 
-namespace App.Services.RequestServices
+namespace App.Services.Factories
 {
     public class ArticleViewModelFactory : IArticleViewModelFactory
     {
         public IEnumerable<ArticleViewModel> Create(string[] results)
         {
             throw new System.NotImplementedException();
+        }
+
+        private ResultViewModel CreateResultVM(string result)
+        {
+            return JsonSerializer.Deserialize<ResultViewModel>(result);
         }
     }
 }
