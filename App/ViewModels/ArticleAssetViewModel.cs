b3276fe0d46bf5e41f3d049fa3eb365aeb1ff207
Márton Szabó <makotoimx@gmail.com>
2021-02-23 13:45:29 +0100

    Create ArticleAssetViewModel

---
+++ App/ViewModels/ArticleAssetViewModel.cs
@@ -0,0 +1,9 @@
+namespace App.ViewModels
+{
+    public struct ArticleAssetViewModel
+    {
+        public string File { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
