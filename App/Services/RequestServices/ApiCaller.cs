3b1845c604ef98ecb8e4dfa65ff62efac73aa348
Márton Szabó <makotoimx@gmail.com>
2021-02-23 14:14:45 +0100

    Add ApiCaller.CreateClient()

--- App/Services/RequestServices/ApiCaller.cs
+++ App/Services/RequestServices/ApiCaller.cs
@@ -1,12 +1,24 @@
+using System.Net.Http;
 using App.ViewModels;
 
 namespace App.Services.RequestServices
 {
     public class ApiCaller : IApiCaller
     {
+
+        private static HttpClient Client;
+
         public ArticleViewModel CallAPI(string querystring)
         {
             throw new System.NotImplementedException();
+        }
+
+        private void CreateClient()
+        {
+            if (Client == null)
+            {
+                Client = new HttpClient();
+            }
         }
     }
 }
