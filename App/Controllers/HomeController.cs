e2075284d409fa22afc83cd3f7bd57e335cb12a1
Márton Szabó <makotoimx@gmail.com>
2021-03-04 10:20:04 +0100

    Revert "Add Newtonsoft.Json to Tests project"
    This reverts commit 05bf8a2c6a1b8a6b92a6721cb6f675c0ca70b577.

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -2,43 +2,25 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using App.Services.ResponseProcessor;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using App.Models;
-using App.Services.ApiRequest;
-using App.Models.ViewModels;
-using App.Services.ResponseProcessor.Interfaces;
 
 namespace App.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IApiRequestService _reqService;
-        private readonly IResponseProcessorService _respProc;
 
-        public HomeController(ILogger<HomeController> logger, IApiRequestService reqService, IResponseProcessorService respProc)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _reqService = reqService;
-            _respProc = respProc;
         }
 
         public IActionResult Index()
         {
-            var models = _reqService.Call().Result;
-
-            MultipleArticleVM mAVM = _respProc.ProcessResponse(models);
-
-            return View(mAVM);
-        }
-
-        public string Test()
-        {
-            var models = _reqService.Call().Result;
-            return models.ToArray()[0].Type;
+            return View();
         }
 
         public IActionResult Privacy()
