622a2413d7e1ed6da0899529f681ef44bb4d4672
Márton Szabó <makotoimx@gmail.com>
2021-03-04 10:19:04 +0100

    Add url creator test

---
+++ Tests/UrlCreatorTests.cs
@@ -0,0 +1,22 @@
+using App.Services.ApiRequest;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class UrlCreatorTests
+    {
+        [Test]
+        public void GetBaseUri_RetunsBaseUriString()
+        {
+            // Arrange
+            IUrlCreator urlCreator = new UrlCreator();
+            string expected = "https://content.guardianapis.com/search?show-blocks=all&api-key=7a75b617-4388-40dc-80f3-b15f4e0e42f6";
+
+            // Act
+            string result = urlCreator.GetBaseUri();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
