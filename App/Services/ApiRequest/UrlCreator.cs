7581f707c5107552569ac2dae5c922cfe715b395
Márton Szabó <makotoimx@gmail.com>
2021-03-10 21:15:25 +0100

    Add page limit to api request

--- App/Services/ApiRequest/UrlCreator.cs
+++ App/Services/ApiRequest/UrlCreator.cs
@@ -5,7 +5,7 @@ namespace App.Services.ApiRequest
         private const string _ApiKey = "7a75b617-4388-40dc-80f3-b15f4e0e42f6"; 
         public string GetBaseUri()
         {
-            return $"https://content.guardianapis.com/search?show-blocks=all&api-key={_ApiKey}&show-fields=all";
+            return $"https://content.guardianapis.com/search?show-blocks=all&api-key={_ApiKey}&show-fields=all&page-size=12";
         }
     }
 }
