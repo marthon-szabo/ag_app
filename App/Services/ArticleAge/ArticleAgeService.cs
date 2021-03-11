b297cd7e494e2bb91097ce789717c0854134145c
Márton Szabó <makotoimx@gmail.com>
2021-03-11 13:41:30 +0100

    Implement CountMyAge()

--- App/Services/ArticleAge/ArticleAgeService.cs
+++ App/Services/ArticleAge/ArticleAgeService.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Collections.Generic;
 using System;
 using App.Models.ViewModels;
 using App.Services.ArticleAge.Interfaces;
@@ -8,7 +10,44 @@ namespace App.Services.ArticleAge
     {
         public string CountMyAge(ArticleVM articleVM)
         {
-            throw new NotImplementedException();
+            if(articleVM.LastModified == null)
+            {
+                return "No date available";
+            }
+
+            DateTime now = DateTime.Now;
+            DateTime lastModification = (DateTime)articleVM.LastModified;
+
+            TimeSpan span = now.Subtract(lastModification);
+
+            return ProcessDates(span);
         }
+
+        private string ProcessDates(TimeSpan span)
+        {
+            StringBuilder result = new StringBuilder();
+
+            IDictionary<string, int> timeUnits = new Dictionary<string, int>()
+            {
+                {"days", span.Days},
+                {"hours", span.Hours},
+                {"minutes", span.Minutes}
+            };
+
+            foreach(string key in timeUnits.Keys)
+            {
+                int currentValue = timeUnits[key]; 
+                
+                if ( currentValue > 0)
+                {
+                    string timeUnit = (!key.Equals("minutes")) ? $"{currentValue} {key}, " : $"{currentValue} {key} ";
+                    result.Append(timeUnit);
+                }
+            }
+
+            result.Append("ago.");
+
+            return result.ToString();
+        } 
     }
 }
